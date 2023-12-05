4b7d19bce78dc1a6d9dbe2d8954ba1ea97f72a1a
Lauri Ahonen <lauri.ahonen@diktamen.com>
2023-12-05 12:29:58 +0200

    Use JAVA_HOME per define

--- buildrepro.cmd
+++ buildrepro.cmd
@@ -2,12 +2,12 @@ rm -rf build
 mkdir build
 cd build
 cl  /DUNICODE /D_UNICODE  /EHsc ..\src\WaitFor.cpp
-javac -d . ..\src\Main.java
-jar cfe Hello.jar Main *.class
+"%JAVA_HOME%\bin\javac.exe" -d . ..\src\Main.java
+"%JAVA_HOME%\bin\jar.exe" cfe Hello.jar Main *.class
 cd ..
 rm -rf package
 mkdir package
 cd package
-jpackage --input ..\build  --name HelloWorld  --main-jar Hello.jar   --main-class Main   --type app-image
+"%JAVA_HOME%\bin\jpackage.exe" --input ..\build  --name HelloWorld  --main-jar Hello.jar   --main-class Main   --type app-image
 cd ..
 
